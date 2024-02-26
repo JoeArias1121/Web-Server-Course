87f082db4bb68c1650f640e7f1e4167035c50813
JoeArias1121 <joearias1121@gmail.com>
2024-02-26 15:17:54 -0500

    Added Vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,21 +1,18 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
+import HelloWorld from './pages/Home.vue'
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
-  <header>
+  <div class="container">
+    <NavBar />
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
     </div>
-  </header>
+  </div>
 
   <RouterView />
 </template>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from '../pages/Home.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <Home />
+  </div>
 </template>
